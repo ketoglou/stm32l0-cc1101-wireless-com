4bd0b7d6b23282620de4bf8c2e4e03a2077f22ea
ketoglou <ketoglou@hotmail.com>
2021-10-27 14:38:50 +0300

    Wed Oct 27 02:38:50 PM EEST 2021

--- stm32l011k4_cc1101_wireless_com/Src/main.c
+++ stm32l011k4_cc1101_wireless_com/Src/main.c
@@ -22,11 +22,10 @@
 #define MSG_BYTES			7															//Number of clear message bytes
 
 //Variables
-uint8_t msg[TOTAL_MSG_BYTES] = {MSG_BYTES,'H', 'e', 'l', 'l', 'o', ' ', '1'};			//Message
+uint8_t msg[TOTAL_MSG_BYTES] = { MSG_BYTES, 'H', 'e', 'l', 'l', 'o', ' ', '1' };//Message
 
 //**************************************************************************************************************************************************************
-int main(void)
-{
+int main(void) {
 
 	init_emb_flash_mem();			//Initialize latency WS
 	init_rcc();						//Initialize clock (RCC)
@@ -40,15 +39,17 @@ int main(void)
 
 	//CC1101 Receive Mode Enable
 	USER_BUFFER[0] = SRX;
-	spi_transmit_wait(USER_BUFFER,1,CC1101_NSS);
+	spi_transmit_wait(USER_BUFFER, 1, CC1101_NSS);
 
-	while(1){
+	while (1) {
 
 		//Monitor if we receive something
-		if(FLAG_READ(FLAG1)){
+		if (FLAG_READ(FLAG1)) {
 			//Receive data from CC1101 (USER_BUFFER[0] is garbage data because we send a command first)
 			//Data start from SPI_RX_BUFFER[1] and there are MSG_BYTES_NUM bytes in number.
-			spi_transmit_wait(USER_BUFFER, init_receive_packet(USER_BUFFER,TOTAL_MSG_BYTES),CC1101_NSS);
+			spi_transmit_wait(USER_BUFFER,
+					init_receive_packet(USER_BUFFER, TOTAL_MSG_BYTES),
+					CC1101_NSS);
 			//Change LED status
 			GPIOB->ODR ^= GPIO_ODR_OD3;
 
@@ -58,14 +59,14 @@ int main(void)
 			//Enable Receive mode again
 			USER_BUFFER[0] = SFRX; //Clear RX buffer
 			USER_BUFFER[1] = SRX; //Go to Receive Mode
-			spi_transmit_wait(USER_BUFFER,2,CC1101_NSS);
+			spi_transmit_wait(USER_BUFFER, 2, CC1101_NSS);
 		}
 
 		//Monitor if the button is pressed
-		if(FLAG_READ(FLAG3)){
+		if (FLAG_READ(FLAG3)) {
 
 			//Send the message
-			cc1101_transmit(msg,TOTAL_MSG_BYTES);
+			cc1101_transmit(msg, TOTAL_MSG_BYTES);
 
 			//Clear Flag
 			FLAG_CLEAR(FLAG3);
@@ -73,7 +74,7 @@ int main(void)
 			//Enable Receive mode again
 			USER_BUFFER[0] = SFRX; //Clear RX buffer
 			USER_BUFFER[1] = SRX; //Go to Receive Mode
-			spi_transmit_wait(USER_BUFFER,2,CC1101_NSS);
+			spi_transmit_wait(USER_BUFFER, 2, CC1101_NSS);
 		}
 
 		//Clear WatchDog (Comment in DEBUG mode)
